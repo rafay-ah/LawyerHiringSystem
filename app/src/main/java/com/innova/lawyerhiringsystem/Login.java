918fe546cdae80e15e821a3485b414c31e81d25b
rafay-ah <54492363+rafay-ah@users.noreply.github.com>
2021-08-31 16:13:18 +0500

    Firebase Auth Login by Password added.

--- app/src/main/java/com/innova/lawyerhiringsystem/Login.java
+++ app/src/main/java/com/innova/lawyerhiringsystem/Login.java
@@ -5,6 +5,7 @@ package com.innova.lawyerhiringsystem;
 * Firebase email Auth is incorporated
 * Forgot Password screen is linked
 * */
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialog;
@@ -17,6 +18,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
@@ -24,6 +26,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -154,6 +163,32 @@ public class Login extends AppCompatActivity {
 
     //all login connectivity will be performed here
     public void login(String email, String password){
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Singin", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Login.this, "Login successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, ClientDashboard.class));
+                            finish();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Singin", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            showItems();
+//                            updateUI(null);
+                        }
+                    }
+                });
 
     }
 }
