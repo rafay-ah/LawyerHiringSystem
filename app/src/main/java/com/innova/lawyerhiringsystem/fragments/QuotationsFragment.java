5713dc0e6b3d8a85485b0ea23c6800716f72756a
rafay-ah <54492363+rafay-ah@users.noreply.github.com>
2021-09-11 22:00:49 +0500

    Client can view Quotations.

--- app/src/main/java/com/innova/lawyerhiringsystem/fragments/QuotationsFragment.java
+++ app/src/main/java/com/innova/lawyerhiringsystem/fragments/QuotationsFragment.java
@@ -1,20 +1,42 @@
 package com.innova.lawyerhiringsystem.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.innova.lawyerhiringsystem.R;
+import com.innova.lawyerhiringsystem.WelcomeScreen;
+import com.innova.lawyerhiringsystem.adapter.ViewQuotations;
+import com.innova.lawyerhiringsystem.model.Case;
+import com.innova.lawyerhiringsystem.model.Quotation;
+
+import java.util.ArrayList;
 
 public class QuotationsFragment extends Fragment {
+
+    EditText query;
+    String tittle;
+    public static ArrayList<Quotation> quotations;
+    Quotation quotation;
+
     public QuotationsFragment() {
         // Required empty public constructor
     }
@@ -37,10 +59,52 @@ public class QuotationsFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootview =  inflater.inflate(R.layout.fragment_quotations, container, false);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(),R.layout.quotations_row);
+        Button button = (Button) rootview.findViewById(R.id.search_case_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { // event listener for search button
+                // logic-> get entered case tittle - search quotation for that case tittle - populate list view with details
+                query = rootview.findViewById(R.id.search_case);
+                tittle = query.getText().toString();
+                quotations = new ArrayList<Quotation>();
 
-        ListView listView = (ListView) rootview.findViewById(R.id.view_quotations);
-        listView.setAdapter(adapter);
+                // now query DB for quotation matching entered tittle
+                final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference ref = database.getReference("quotations");
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                            for (DataSnapshot innerChild : child.getChildren())
+                            {
+                                quotation = innerChild.getValue(Quotation.class);
+
+                                if (quotation.getCaseTittle().equals(tittle)){
+                                    // we have found the instance of quotation for case
+                                    Log.i("casequote", quotation.getLawyerid());
+                                    quotations.add(quotation);
+
+                                }
+                            }
+                        }
+                        // Create the adapter to convert the array to views
+                        ViewQuotations adapter = new ViewQuotations(getActivity(), quotations);
+                        // Attach the adapter to a ListView
+                        ListView listView =  rootview.findViewById(R.id.view_quotations);
+                        listView.setAdapter(adapter);
+//                        // only populate listview if it is not the first time fragment is created
+//                        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(),
+//                                R.layout.available_case_row, caseNames);
+//                        listCases.setAdapter(adapter);
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        System.out.println("The read failed: " + databaseError.getCode());
+                    }
+                });
+            }
+        });
         return rootview;
     }
 
